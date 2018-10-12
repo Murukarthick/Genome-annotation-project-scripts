45950eaafc6b7368d3193e67774c8fa1fec806d6
Murukarthick Jayakodi <42235985+Murukarthick@users.noreply.github.com>
2018-10-12 16:03:05 +0200

    Consensus sequence from vcf file

--- consensus_seq_vcf.zsh
+++ consensus_seq_vcf.zsh
@@ -1,10 +1,11 @@
 # Consensus sequence extraction
 # Programs
-samtools='/opt/Bio/samtools/1.9/bin/samtools'
-bcftools='/opt/Bio/bcftools/1.9/bin/bcftools'
-ref='/filer-dg/agruppen/seq_shared/mascher/wheat_psmol_v2_160618/161010_CS_v1.0_pseudomolecules.fasta'
+# Provide absolute path
+samtools='/source/samtools/1.9/bin/samtools'
+bcftools='/source/bcftools/1.9/bin/bcftools'
+ref='pseudomolecules.fasta'
 
-# Extract for specific regions
+# Extract consensus sequence for specific regions
 # Use -H opiton when you have multiple alt allel
 
-$samtools faidx $ref chr7B:5059617-50604264 | $bcftools consensus -s TRI-10703-13 -o TRI-10703-13.fa AE_5310-10703_WGS_samtools.vcf.gz
+$samtools faidx $ref chr7B:5059617-50604264 | $bcftools consensus -s TRI-10703-13 -o TRI-10703-13.fa WGS_samtools.vcf.gz
